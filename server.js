a689728eef67ba851358d58bbb35cfde0f0bff75
Din <rivojidin97@gmail.com>
2024-05-14 21:10:37 +0900

    feat: develop author router ejs page

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ console.log("Web Serverni boshlash!");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
 
+let user;
+
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("err", err);
+  } else {
+    user = JSON.parse(data);
+  }
+});
+
+console.log(express.static("public"));
 // 1: Kirish code
 app.use(express.static("public"));
 app.use(express.json());
@@ -16,18 +28,21 @@ app.set("view engine", "ejs");
 
 // 4: Routing code
 
-app.post('/create-item', (req, res)=> {
+app.post("/create-item", (req, res) => {
   console.log(req);
-  res.json({test: "success"})
-})
-
-app.get("/", function(req, res) {
-  res.render('harid');
+  res.json({ test: "success" });
 });
 
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+});
+
+app.get("/", function (req, res) {
+  res.render("harid");
+});
 
 const server = http.createServer(app);
 let PORT = 3000;
-server.listen(PORT, ()=>{
+server.listen(PORT, () => {
   console.log(`The server is running successfully on port: ${PORT}`);
 });
